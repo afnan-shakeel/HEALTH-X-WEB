b916ee3a4c692ccf5fa5fa25cdc99012eeef1201
afnan-shakeel <afnanshakeel@gmail.com>
2023-12-28 01:15:17 +0530

    login page - updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require("tw-elements/dist/plugin.cjs")
   ],
 }
 
